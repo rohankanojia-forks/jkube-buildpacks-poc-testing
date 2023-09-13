e7679685b8c27aaa5f6f78d60f9ad160d33ef5b3
Rohan Kumar <rohaan@redhat.com>
2023-09-13 14:21:26 +0530

    Initial Commit
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ maven-it/src/main/java/org/eclipse/jkube/demos/springboot3demojkube/SpringBoot3DemoJkubeApplication.java
@@ -0,0 +1,13 @@
+package org.eclipse.jkube.demos.springboot3demojkube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3DemoJkubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3DemoJkubeApplication.class, args);
+	}
+
+}
